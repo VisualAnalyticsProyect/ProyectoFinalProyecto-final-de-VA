4bd3fe32b8c996fbb6c2571891abc16e5799a91a
Tizon <tivetind23@hotmail.com>
2016-11-02 22:53:15 -0500

    test publicación

--- ESATD3/ESATD3/public/javascripts/visualD3.js
+++ ESATD3/ESATD3/public/javascripts/visualD3.js
@@ -23,7 +23,7 @@ var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-10, 0])
     .html(function (d) {
-        return "<strong>Frequency:</strong> <span style='color:red'>" + d.PROMEDIO + "</span>";
+        return "<strong>Calificación:</strong> <span style='color:red'>" + d.PROMEDIO + "</span>";
     })
 
 var svg = d3.select("body").append("svg")
